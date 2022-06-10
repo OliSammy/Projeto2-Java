f14cd24f442d312f2d3fb24a9422cc04391df6f2
Matheus Freitas <freitas.matheus.profissional@outlook.com>
2022-06-10 19:12:15 -0300

    Nova classe de exceção, GabaritoInvalidoException

--- src/domainexception/DisciplinaVaziaException.java
+++ src/domainexception/DisciplinaVaziaException.java
@@ -1,5 +1,7 @@
 package domainexception;
 
 public class DisciplinaVaziaException extends Exception {
-
+    public DisciplinaVaziaException() {
+        super("Disciplina encontrasse vazia no momento. \nCadastre alunos.");
+    }
 }
