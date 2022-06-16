d8056cf6d6255fb7308f8c764d34d614f5d1d65f
Matheus Freitas <freitas.matheus.profissional@outlook.com>
2022-06-16 12:40:32 -0300

    Finalizado, esperando os testes.

--- src/App.java
+++ src/App.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
+import domainexception.GabaritoInvalidoException;
 import entities.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner ler = new Scanner(System.in);
+        Scanner lerNum = new Scanner(System.in);
         Cadeiras cadeiras = new Cadeiras();
         String nome;
         int id;
@@ -12,13 +14,14 @@ public class App {
         Disciplina disciplina;
         System.out.println(
                 "Selecione a opção que deseja realizar:\n1.Cadastrar novadisciplina\t2.Acessar disciplina\t3.Sair do programa.");
-        int op = ler.nextInt();
+        int op = lerNum.nextInt();
         while (op != 3) {
             switch (op) {
                 case 1:
-                    System.out.println("\nDigite o nome da disciplina e a id que deseja associar a ela.");
-                    nome = ler.next();
-                    id = ler.nextInt();
+                    System.out.println("\nDigite o nome da disciplina:");
+                    nome = ler.nextLine();
+                    System.out.println("Digite a id que deseja associar a ela:");
+                    id = lerNum.nextInt();
                     disciplina = new Disciplina(nome, id);
                     cadeiras.adicionarDisciplina(disciplina);
                     break;
@@ -37,17 +40,17 @@ public class App {
                         System.out.println("\nDisciplina " + disciplina.getNomeDisciplina() + " selecionada.");
                         System.out.println(
                                 "\n1.Cadastrar Alunos e notas\t2.Gerar notas\t3.Consultar notas\t4.Sair do menu disciplina");
-                        int op2 = ler.nextInt();
+                        int op2 = lerNum.nextInt();
                         while (op2 != 4) {
                             switch (op2) {
                                 case 1:
                                     System.out.println("Quantos alunos deseja cadastrar?");
-                                    int qtde = ler.nextInt();
+                                    int qtde = lerNum.nextInt();
                                     for (int i = 0; i < qtde; i++) {
                                         System.out.println(
                                                 "\nDigite o nome, a matricula e as respostas do aluno a ser cadastrado");
                                         nome = ler.next();
-                                        id = ler.nextInt();
+                                        id = lerNum.nextInt();
                                         respostas = ler.next();
                                         Aluno aluno = new Aluno(nome, id, respostas);
                                         disciplina.adicionarAluno(aluno);
@@ -57,18 +60,26 @@ public class App {
                                     System.out.println(
                                             "\nDigite o caminho do arquivo de gabarito. Certifique se de digitar o caminho corretamente e com duas barras '\\' ");
                                     String caminho = ler.next();
-                                    disciplina.receberGarabito(caminho);
                                     try {
-                                        disciplina.gerarRespostasTurma();
+                                        disciplina.receberGarabito(caminho);
+                                        try {
+                                            disciplina.gerarRespostasTurma();
+                                            System.out.println("Gerando notas...");
+                                            for (int i = 0; i < 10; i++) {
+                                                System.out.print("#");
+                                                Thread.sleep(1000);
+                                            }
+                                            System.out.println("\nNotas geradas com sucesso.\n");
+                                        } catch (Exception e) {
+                                            System.out.println("Error: " + e.getMessage());
+                                        }
+                                    } catch (GabaritoInvalidoException e) {
+                                        System.out.println(e.getMessage());
                                     } catch (Exception e) {
-                                        System.out.println("Error: " + e.getMessage());
+                                        System.out.println(
+                                                "Não foi possível encontrar o arquivo. Verifique se você escreveu corretamente o caminho, e tente novamente.");
                                     }
-                                    System.out.println("Gerando notas...");
-                                    for (int i = 0; i < 10; i++) {
-                                        System.out.print("#");
-                                        Thread.sleep(1000);
-                                    }
-                                    System.out.println("\nNotas geradas com sucesso.\n");
+
                                     break;
                                 case 3:
                                     try {
@@ -80,21 +91,23 @@ public class App {
                                     }
                                     break;
                                 default:
+                                    System.out.println("Opção inválida.");
                                     break;
                             }
                             System.out.println(
                                     "\n1.Cadastrar Alunos e notas\t2.Gerar notas\t3.Consultar notas\t4.Sair do menu disciplina");
-                            op2 = ler.nextInt();
+                            op2 = lerNum.nextInt();
                         }
                     }
-
                 default:
+                    System.out.println("Opção inválida.");
                     break;
             }
             System.out.println(
                     "\nSelecione a opção que deseja realizar:\n1.Cadastrar nova disciplina\t2.Acessar disciplina\t3.Sair do programa.");
-            op = ler.nextInt();
+            op = lerNum.nextInt();
         }
         ler.close();
+        lerNum.close();
     }
 }
